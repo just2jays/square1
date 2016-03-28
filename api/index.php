85f353f535d57384435209256d0f1f8fd00d3469
JJ <just2jays@gmail.com>
2016-03-28 01:04:03 -0400

    thats all

--- api/index.php
+++ api/index.php
@@ -143,6 +143,7 @@ class Rest {
         foreach ($this->db->query("SELECT checkin.id as 'checkin_id', checkin.checkin_latitude, checkin.checkin_longitude, place.place_name FROM checkin INNER JOIN place ON checkin.checkin_place_id = place.id ORDER BY checkin.id DESC LIMIT 10") as $row) {
             $response[] = array(
                 'ID' => $row['checkin_id'],
+                'name' => $row['place_name'],
                 'latitude' => $row['latitude'],
                 'longitude' => $row['longitude']
             );
