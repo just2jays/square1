0cf29628e303fbbfbceb49bc44d58eaf6d20019f
JJ <just2jays@gmail.com>
2016-03-28 01:01:22 -0400

    ugh I'm sorry

--- api/index.php
+++ api/index.php
@@ -140,7 +140,7 @@ class Rest {
     }
 
     public function getCheckinCollection() {
-        foreach ($this->db->query("SELECT checkin.id as 'checkin_id', checkin.latitude, checkin.longitude, place.place_name FROM checkin INNER JOIN place ON checkin.checkin_place_id = place.id ORDER BY checkin.id DESC LIMIT 10") as $row) {
+        foreach ($this->db->query("SELECT checkin.id as 'checkin_id', checkin.checkin_latitude, checkin.checkin_longitude, place.place_name FROM checkin INNER JOIN place ON checkin.checkin_place_id = place.id ORDER BY checkin.id DESC LIMIT 10") as $row) {
             $response[] = array(
                 'ID' => $row['checkin_id'],
                 'latitude' => $row['latitude'],
