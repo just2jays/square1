09b87646d00014fa3a49c6c71aa7cf7d63af6aff
JJ <just2jays@gmail.com>
2016-03-29 23:16:06 -0400

    damm

--- js/views/appContainer.js
+++ js/views/appContainer.js
@@ -2,9 +2,7 @@ var AppContainerView = Backbone.View.extend({
      myChildView: null,
 
      render: function() {
-        //this.$el.html('<div id=""></div><div class="">');
-
-        this.$el.html(this.myChildView.$el);
+        this.$el.append(this.myChildView.$el);
         return this;
     }
 });
