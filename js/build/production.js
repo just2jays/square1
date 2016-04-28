03b831850bc28d7155bfbb0241328488eece45fa
JJ Guaragno <jguaragno@inc.com>
2016-04-28 18:37:39 -0400

    asdf

--- js/build/production.js
+++ js/build/production.js
@@ -161,6 +161,7 @@ var CheckinView = Backbone.View.extend({
      *     as a response from getGeoInfo();
      */
     fetchNearbyFoursquareVenues: function(position){
+        console.log(position);
     	latitude = position.coords.latitude;
     	longitude = position.coords.longitude;
     	var d = new Date();
