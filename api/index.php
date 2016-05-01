df9c9ad3a743b483a041215eda76856b43752a48
JJ <just2jays@gmail.com>
2016-05-01 18:30:49 -0400

    awful

--- api/index.php
+++ api/index.php
@@ -135,9 +135,6 @@ class Rest {
     }
 
     public function loginUser($data){
-        error_log("weaiofjaweir");
-        error_log("SELECT * FROM user WHERE username=\"$data['username']\" LIMIT 1");
-        error_log(print_r($data,true));
         $stmt = $this->db->prepare("SELECT * FROM user WHERE username = \"".$data['username']."\" LIMIT 1");
         $result = $stmt->execute();
         if($result) {
