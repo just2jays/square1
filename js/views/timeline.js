ba5cb870454fa9658c28c99a684810d42b576f14
JJ Guaragno <jguaragno@inc.com>
2016-05-02 15:13:35 -0400

    saving is good

--- js/views/timeline.js
+++ js/views/timeline.js
@@ -24,6 +24,7 @@ var MapView = Backbone.View.extend({
             });
         }else{
             this.$el.html( '<div class="container"><div class="row"><div class="col-sm-8 col-sm-offset-2 text-center">Please Login</div></div></div>' );
+            window.location.hash = 'login';
         }
     },
 
