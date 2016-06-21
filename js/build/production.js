21064e1b8f4dfd6952f7fb61d69267f0f9d8953f
JJ <just2jays@gmail.com>
2016-06-21 01:34:49 -0400

    nababa

--- js/build/production.js
+++ js/build/production.js
@@ -459,9 +459,8 @@ var InventoryView = Backbone.View.extend({
     payForPrize: function() {
         $('#payForPrizeModal .the-gift-cover .the-gift').addClass('animated hinge');
         $.get( 'api/Utilities/forcePrize/'+appUser.id, _.bind(function(data) {
-            console.log(data);
             $('#payForPrizeModal .modal-header h4').html('WOW! Nice find!');
-            $('#payForPrizeModal .the-gift-reveal .prize-well').html('<div class="prizeBox"><img src="'+data.item.image+'" /><div class="item-name">'+data.item.name+' #'+data.unique+'</div></div>');
+            $('#payForPrizeModal .the-gift-reveal .prize-well').html('<div class="prizeBox"><img src="'+data.prize.item.image+'" /><div class="item-name">'+data.prize.item.name+' #'+data.prize.item.unique+'</div></div>');
             $('#payForPrizeModal .the-gift-cover').fadeOut();
             $('#payForPrizeModal .the-gift-reveal').fadeIn();
             appUser.fetchInventory();
