e95ebfb4c12e9ac169debfa85385afa978fe466f
JJ <just2jays@gmail.com>
2016-07-04 23:53:14 -0400

    makes more sense

--- js/views/checkin.js
+++ js/views/checkin.js
@@ -165,8 +165,8 @@ var CheckinView = Backbone.View.extend({
     },
 
     handleIncludedPhoto: function(e){
-        console.dir(e.currentTarget.files);
-        $('.include-checkin-photo-btn').removeClass('.btn-primary').addClass('.btn-success');
-        $('.include-checkin-photo-btn i').html('').html('<i class="fa fa-camera-retro" aria-hidden="true"></i> Picture Added!');
+        console.log(e.currentTarget.files);
+        $('.include-checkin-photo-btn').removeClass('btn-primary').addClass('btn-success');
+        $('.include-checkin-photo-btn').html('').html('<i class="fa fa-camera-retro" aria-hidden="true"></i> Picture Added!');
     }
 });
