ad1d8520011095dd12abdb6d3fd999134f4f0a2e
JJ <just2jays@gmail.com>
2016-07-05 01:01:36 -0400

    asdf

--- js/views/checkin.js
+++ js/views/checkin.js
@@ -4,7 +4,7 @@ var CheckinView = Backbone.View.extend({
     initialize: function(){
         this.venues = [];
         this.selectedVenue = {};
-        this.includedPhoto = new FormData();
+        this.includedPhoto = new FileReader();
         this.render();
     },
 
@@ -167,7 +167,7 @@ var CheckinView = Backbone.View.extend({
 
     handleIncludedPhoto: function(e){
         //console.log(e.currentTarget.files);
-        this.includedPhoto.append('file', e.currentTarget.files[0]);
+        this.includedPhoto.readAsDataURL(e.currentTarget.files[0]);
         $('.include-checkin-photo-btn').removeClass('btn-primary').addClass('btn-success');
         $('.include-checkin-photo-btn').html('').html('<i class="fa fa-camera-retro" aria-hidden="true"></i> Picture Added!');
     }
