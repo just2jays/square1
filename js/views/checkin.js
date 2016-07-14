b70e3334e9d6b51e246263a5aa86ec7782bb3308
JJ Guaragno <jguaragno@inc.com>
2016-07-14 13:14:43 -0400

    proper naming

--- js/views/checkin.js
+++ js/views/checkin.js
@@ -173,7 +173,7 @@ var CheckinView = Backbone.View.extend({
 
         // Upload to Uploadcare service
         var checkinImage = uploadcare.fileFrom('object', e.currentTarget.files[0]);
-        file.progress(function(uploadInfo) {
+        checkinImage.progress(function(uploadInfo) {
             console.log(uploadInfo.progress);
         });
         checkinImage.done(_.bind(function(fileInfo) {
